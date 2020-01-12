f767c95549d8d718d6073632f043fe4badf47d7f
TheGrunger2000 <53703277+TheGrunger2000@users.noreply.github.com>
2020-01-12 19:28:08 +0300

    Telebot was Added

--- client/Client.py
+++ client/Client.py
@@ -1,29 +1,32 @@
+import telebot
+
+
 class Client:
 
     def __init__(self):
-        self._message = None
+        self._bot_token = '809780880:AAHX84SLr1b_NAgpD_TqgOC_ERW1PkA19pw'
+        self.bot = telebot.TeleBot(self._bot_token)
+
         self._server = None
-
-    def print_message(self):
-        print(self._message)
-
-    @property
-    def message(self):
-        return self._message
 
     @property
     def server(self):
         return self._server
-
-    @message.setter
-    def message(self, message):
-        self._message = message
 
     @server.setter
     def server(self, server):
         self._server = server
 
     def run(self, token, lang="en", session_id="aibot"):
-        self._server.answer = self._message
-        self._server.run(token, lang, session_id)
-        self._message = self._server.answer
+        @self.bot.message_handler(commands=['start'])
+        def start_message(message):
+            self.bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+        @self.bot.message_handler(content_types=['text'], commands=['random'])
+        def send_text(message):
+            self._server.answer = message
+            self._server.run(token, lang, session_id)
+            answer = self._server.answer
+            self.bot.send_message(message.chat.id, answer)
+
+        self.bot.polling()
