61a3513fc948ca2a7e9c2b19dd671e2eddecb5db
TheGrunger2000 <53703277+TheGrunger2000@users.noreply.github.com>
2020-01-14 21:25:15 +0300

    We did it - it searching

--- ai_message.py
+++ ai_message.py
@@ -8,23 +8,26 @@ TOKEN = "TOKEN"
 
 
 def ai_message(message, lang="ru", session_id="aibot"):
+
     assert message is not None
     request = apiai.ApiAI(TOKEN).text_request()
     request.lang = lang
     request.session_id = session_id
-    request.query = message.replace("/d" or "/bot", "")
+    request.query = message.replace("/bot", "")
 
     responseJson = json.loads(request.getresponse().read())
     pprint.pprint(responseJson)
     answer_text = responseJson['result']['fulfillment']['speech']
-    what = "+".join(message.replace("/d" or "/bot", "").split(" ")[1:])
-
+    what = "+".join(message.replace("/bot", "").split(" ")[1:])
     search_data = responseJson['result']['parameters']
 
-    if search_data['google_search'] == ['search']:
-        answer = f'{answer_text}\nhttps://www.google.com/search?ei=Qb8cXqeHHdD76QTxqZHABw&q={what}'
-    elif search_data['youtube_search'] == ['video']:
-        answer = f"https://www.youtube.com/results?search_query={what}"
+    answer = None
+    if 'google_search' in search_data:
+        if search_data['google_search'] == ['search'] or 'search':
+            answer = f'{answer_text}\nhttps://www.google.com/search?ei=Qb8cXqeHHdD76QTxqZHABw&q={what}'
+    elif 'youtube_search' in search_data:
+        if search_data['youtube_search'] == ['video'] or 'video':
+            answer = f'{answer_text}\nhttps://www.youtube.com/results?search_query={what}'
     else:
         answer = answer_text
     return answer
